8537178729b23b06cfdac19b879e30ef07295c72
Alejandro Elgueta <alejandro.elgueta@gmail.com>
2021-01-19 20:52:55 -0300

    test alejo

--- src/test/java/ejemplopipeline/DiezxcientoTest.java
+++ src/test/java/ejemplopipeline/DiezxcientoTest.java
@@ -9,17 +9,36 @@ public class DiezxcientoTest {
 	@Test
 	public void testDiezxciento() throws Exception {
 		Diezxciento diezxciento = new Diezxciento(23000000,900000);
-		assertEquals(2400000, diezxciento.diezxciento());
+		assertEquals(2300000, diezxciento.diezxciento());
 	}
+
+        @Test
+        public void testDiezxciento() throws Exception {
+                Diezxciento diezxciento = new Diezxciento(14000000,500000);
+                assertEquals(1400000, diezxciento.diezxciento());
+        }
+
+       @Test
+        public void testDiezxciento() throws Exception {
+                Diezxciento diezxciento = new Diezxciento(38000000,5000000);
+                assertEquals(3800000, diezxciento.diezxciento());
+        }
+
 
 	@Test
 	public void testImpuesto() throws Exception {
+		assertEquals(3800000, testimpuesto();
 
 	}
 
 	@Test
 	public void testSaldoAhorro() throws Exception {
+		SaldoAhorro saldoahorro = new SaldoAhorro(1000000);
+		asserEquals(9000000, testSaldoAhorro();
 
 	}
+
+
+
 
 }
